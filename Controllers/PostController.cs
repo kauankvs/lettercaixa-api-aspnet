86f0ad327ba8bdd87af373f036c66d5caeb15238
Kauan Vieira <kauanvieirakvs22@gmail.com>
2023-01-23 15:58:17 -0300

    controller para posts - add e delete

---
+++ Controllers/PostController.cs
@@ -0,0 +1,29 @@
+﻿using LettercaixaAPI.Models;
+using LettercaixaAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace LettercaixaAPI.Controllers
+{
+    [Route("api/post")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostService _service;
+        public PostController(IPostService service) => _service = service;
+
+        [HttpPost]
+        [Route("add")]
+        [Authorize]
+        public async Task<ActionResult<Post>> AddCommentaryToMovieAsync(int movieId, string comment, int score)
+            => await _service.AddCommentaryToMovieAsync(User.FindFirstValue(ClaimTypes.Email), movieId, comment, score);
+
+        [HttpDelete]
+        [Route("delete")]
+        [Authorize]
+        public async Task<ActionResult> RemoveCommentaryToMovieAsync(int movieId)
+            => await _service.RemoveCommentaryToMovieAsync(User.FindFirstValue(ClaimTypes.Email), movieId);
+    }
+}
